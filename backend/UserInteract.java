41e855b7635bdc6228f124d81cb1f569547ee88c
hyup98 <hyup98@naver.com>
2025-02-04 19:53:03 +0900

    log들 주석처리

--- backend/UserInteract.java
+++ backend/UserInteract.java
@@ -18,12 +18,12 @@ public class UserInteract {
     //chef의 행동점수와 dish_idx가 5가 넘는지 체크하는 메서드
     public boolean answerKeepPlaying(){
         if(headChef.getActionPoint() < 1){
-            logger.info("행동 포인트를 모두 소진");
+            //logger.info("행동 포인트를 모두 소진");
             return false;
         }
 
         if(dish_idx>=5){
-            logger.info("dish의 재료를 모두 채움");
+            //logger.info("dish의 재료를 모두 채움");
             return false;
         }
         return true;
@@ -33,37 +33,37 @@ public class UserInteract {
     public boolean answerFoodSelectRequest(String food_name){
         Food food = db.getFoodClass(food_name);
         if(food == null){
-            logger.warning("DB에서 재료 클래스를 불러오는데 실패");
+            //logger.warning("DB에서 재료 클래스를 불러오는데 실패");
             return false;
         }
         this.food = food;
-        logger.info("DB에서 재료 객체를 불러오는데 성공");
+        //logger.info("DB에서 재료 객체를 불러오는데 성공");
         return true;
     }
 
     //현재 고른 재료를 손질하는 함수
     public boolean answerAddStatus(String new_status){
         if(!headChef.cooking(food, new_status)){
-            logger.info(new_status+"추가 실패");
+            //logger.info(new_status+"추가 실패");
             return false;
         }
-        logger.info(food.getName()+"에 "+new_status+"추가 성공");
+        //logger.info(food.getName()+"에 "+new_status+"추가 성공");
         return true;
     }
     //요리시간이 필요한 재료 손질이 들어오면 반영하여 재료 손질
     public boolean answerAddStatus(String new_status, int cookingtime){
         if(!headChef.cooking(food, new_status, cookingtime)){
-            logger.info(new_status+"추가 실패");
+            //logger.info(new_status+"추가 실패");
             return false;
         }
-        logger.info(food.getName()+"에 "+new_status+"추가 성공");
+        //logger.info(food.getName()+"에 "+new_status+"추가 성공");
         return true;
     }
 
     //dish에 food 추가하는 메서드
     public void answerAddDish(){
         dish[dish_idx] = food;
-        logger.info("Dish["+dish_idx+"]에 "+food.getName()+" 추가");
+        //logger.info("Dish["+dish_idx+"]에 "+food.getName()+" 추가");
         dish_idx++;
     }
 
@@ -76,7 +76,7 @@ public class UserInteract {
     //현재 고른 재료의 타입을 반환하는 함수
     public String answerCurrentFoodType(){
         if(food == null){
-            logger.warning("현재 Food클래스가 존재하지 않음");
+            //logger.warning("현재 Food클래스가 존재하지 않음");
             return null;
         }
         return this.food.getType();
@@ -85,12 +85,13 @@ public class UserInteract {
     //현재 고른 재료의 이름을 반환하는 함수
     public String answerCurrentFoodName(){
         if(food == null){
-            logger.warning("현재 Food클래스가 존재하지 않음");
+            //logger.warning("현재 Food클래스가 존재하지 않음");
             return null;
         }
         return this.food.getName();
     }
     public int answerActionPoint(){
+        //logger.info("채점 진행중..");
         return headChef.getActionPoint();
     }
 
